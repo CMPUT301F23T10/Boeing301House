320e31081dc5de166bd4edffb8f3330121e30d51
Roderick Lan <rodericklan03@gmail.com>
2023-10-28 21:22:41 -0600

    Added comments

--- app/src/main/java/com/example/boeing301house/ItemListActivity.java
+++ app/src/main/java/com/example/boeing301house/ItemListActivity.java
@@ -6,7 +6,7 @@ import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ListView;
 import android.widget.TextView;
-import android.widget.Toast;
+import android.widget.Toast; // for testing
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
