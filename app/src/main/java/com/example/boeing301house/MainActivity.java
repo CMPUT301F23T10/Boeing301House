3f0b5604b391724600e9fd3a894f0345a814c035
Andres Gonzalez <58984169+andresgc808@users.noreply.github.com>
2023-11-01 16:41:58 -0600

    Added first launch procedure

--- app/src/main/java/com/example/boeing301house/MainActivity.java
+++ app/src/main/java/com/example/boeing301house/MainActivity.java
@@ -2,27 +2,48 @@ package com.example.boeing301house;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import java.util.UUID;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        // regular procedure
 
-        // first launch procedure below (can also implement helper activity)
+        // first launch procedure below
         SharedPreferences pref = getSharedPreferences("mypref", MODE_PRIVATE);
-
+        // set intent
+        final Intent intent = new Intent(MainActivity.this, ItemListActivity.class);
+        // update firstart value in pref to test functionality
         if(pref.getBoolean("firststart",true)){
-            // after updating sharedpreferences it will not be triggered again
-            // uncomment next 3 lines when ready to update SharedPreferences
-//            SharedPreferences.Editor editor = pref.edit();
-//            editor.putBoolean("firststart", false);
-//            editor.commit(); // apply changes
+            setContentView(R.layout.user_login);
             // first launch procedure below (login screen)
+            final Button signInButton = findViewById(R.id.sign_in_button);
+            signInButton.setOnClickListener(new View.OnClickListener() {
+                public void onClick(View v) {
+                    // after updating sharedpreferences it will not be triggered again
+                    SharedPreferences.Editor editor = pref.edit();
+                    editor.putBoolean("firststart", false);
+
+                    // generate uniqueId and store in editor
+//                    final String userId = getUniqueId();
+//                    editor.putString("userID", userId);
+                    editor.commit(); // apply changes
+                    startActivity(intent);
+                }
+            });
         }
+        else {
+            startActivity(intent);
+        }
+    }
+    private String getUniqueId(){
+        return UUID.randomUUID().toString();
     }
 }
