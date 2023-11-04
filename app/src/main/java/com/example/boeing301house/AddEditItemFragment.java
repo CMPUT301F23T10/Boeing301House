40f348de58c02cd2e714fea828043f58cd3debdd
kyyse <kyyse1026@gmail.com>
2023-11-04 14:36:09 -0600

    Made a new filter fragment.

--- app/src/main/java/com/example/boeing301house/AddEditItemFragment.java
+++ app/src/main/java/com/example/boeing301house/AddEditItemFragment.java
@@ -335,17 +335,7 @@ public class AddEditItemFragment extends Fragment {
 
     }
 
-//    private void filterItemsByDateRange(Long startDate, Long endDate) {
-//        ArrayList<Item> filteredItems = new ArrayList<>();
-//        for (Item item : items) {
-//            if (item.getDate() >= startDate && item.getDate() <= endDate) {
-//                filteredItems.add(item);
-//            }
-//        }
-//        // Now, `filteredItems` contains only the items within the selected date range.
-//        // Update your adapter or list view with this filtered list.
-//        ItemAdapter.updateData(filteredItems);
-//    }
+
     // TODO: finish java doc
     /**
      *
---
+++ app/src/main/java/com/example/boeing301house/filter.java
@@ -0,0 +1,15 @@
+package com.example.boeing301house;
+
+public class filter {
+    //    private void filterItemsByDateRange(Long startDate, Long endDate) {
+//        ArrayList<Item> filteredItems = new ArrayList<>();
+//        for (Item item : items) {
+//            if (item.getDate() >= startDate && item.getDate() <= endDate) {
+//                filteredItems.add(item);
+//            }
+//        }
+//        // Now, `filteredItems` contains only the items within the selected date range.
+//        // Update your adapter or list view with this filtered list.
+//        ItemAdapter.updateData(filteredItems);
+//    }
+}
