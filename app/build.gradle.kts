4d01add9b4911c11da0e143ad59ee7363cca5dde
Roderick Lan <rodericklan03@gmail.com>
2023-11-13 21:39:25 -0700

    added testing repos

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -50,7 +50,7 @@ dependencies {
 
 
     testImplementation("junit:junit:4.13.2")
-    testImplementation("org.mockito:mockito-core:3.4.5")
+    testImplementation("org.mockito:mockito-core:3.+")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
     implementation("org.apache.commons:commons-lang3:3.9")
