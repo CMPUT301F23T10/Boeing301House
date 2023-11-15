573219c0ac0375b8adffec30959f11933d04e348
Roderick Lan <rodericklan03@gmail.com>
2023-11-15 07:19:30 -0700

    controller stuff + db listener

--- app/src/main/java/com/example/boeing301house/ItemList/ItemListController.java
+++ app/src/main/java/com/example/boeing301house/ItemList/ItemListController.java
@@ -1,6 +1,8 @@
 package com.example.boeing301house.ItemList;
 
 import com.example.boeing301house.Item;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.FirebaseFirestore;
 
 import java.util.ArrayList;
 
@@ -12,6 +14,27 @@ public class ItemListController {
 
     private boolean isMultiSelect = false;
 
+    private ItemList itemList; // model
+    FirebaseFirestore db;
+    CollectionReference itemsRef;
+
+
+    /**
+     * Called on startup, data setup
+     */
+    public void onStartup() {
+        db = FirebaseFirestore.getInstance();
+        itemsRef = db.collection("items");
+        itemList = new ItemList(itemsRef); // maybe use db object
+
+
+        return;
+    }
+
+    /**
+     * Item multiselect behavior startup
+     * @param item: selected item
+     */
     public void onMultiSelectStart(Item item) {
         selectedItems.add(item);
         item.select();
@@ -19,7 +42,10 @@ public class ItemListController {
         return;
     }
 
-
+    /**
+     * Item selection behavior
+     * @param item: selected item
+     */
     public void onSelect(Item item) {
         if (isMultiSelect) {
             // multiselect behavior
@@ -29,6 +55,29 @@ public class ItemListController {
         }
         return;
     }
+
+    // TODO: sort, filter, add/edit, update
+
+    /**
+     * Overloaded function for general filtering
+     * Filter by date
+     * @param start: start date
+     * @param end: end date
+     */
+    public void filter(long start, long end) {
+        itemList.filterDate(start, end);
+    }
+
+    /**
+     * Overloaded function for general filtering
+     * Filter by search term (make/desc)
+     * @param search: search term(s)
+     */
+    public void filter(String search) {
+        itemList.filterSearch(search); // not implemented
+    }
+
+
 
 
 
