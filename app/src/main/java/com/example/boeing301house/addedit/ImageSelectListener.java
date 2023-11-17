08cab1fa230af4e7b2e4b2d1b0e82693f68cd72e
Roderick Lan <rodericklan03@gmail.com>
2023-11-17 13:04:21 -0700

    jdocs

--- app/src/main/java/com/example/boeing301house/addedit/ImageSelectListener.java
+++ app/src/main/java/com/example/boeing301house/addedit/ImageSelectListener.java
@@ -6,5 +6,9 @@ import android.net.Uri;
  * interface for selecting img
  */
 public interface ImageSelectListener {
+    /**
+     * Behavior for click
+     * @param pos position of img in recycler
+     */
     void onItemClicked(int pos);
 }
