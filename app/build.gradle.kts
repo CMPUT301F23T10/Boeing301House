0d89c9667b25dab8733ed3376ed7e70e21cb5e2b
Andres Gonzalez <58984169+andresgc808@users.noreply.github.com>
2023-11-17 16:01:38 -0700

    Added Tag Functionality
    User can now add their own tags and the tags are added to the item on confirm.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -45,6 +45,7 @@ dependencies {
     implementation("androidx.navigation:navigation-ui:2.7.5")
     implementation("androidx.navigation:navigation-dynamic-features-fragment:2.7.5")
     implementation("com.google.firebase:firebase-auth:22.3.0")
+    implementation("androidx.coordinatorlayout:coordinatorlayout:1.2.0")
     androidTestImplementation("androidx.navigation:navigation-testing:2.7.5")
     implementation("androidx.navigation:navigation-compose:2.7.5")
 
