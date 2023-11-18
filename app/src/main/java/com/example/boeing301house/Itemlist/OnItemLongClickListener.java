71ca4b0627db52113fa42b211c43e40ae58926a8
Roderick Lan <rodericklan03@gmail.com>
2023-11-18 13:11:47 -0700

    jdoc fix

--- app/src/main/java/com/example/boeing301house/Itemlist/OnItemLongClickListener.java
+++ app/src/main/java/com/example/boeing301house/Itemlist/OnItemLongClickListener.java
@@ -5,14 +5,14 @@ import android.widget.AdapterView;
 
 /**
  * Item long click listener for RecyclerView
- * @return true if the callback consumed the long click, false otherwise
+ *
  */
 public interface OnItemLongClickListener {
     /**
      * Item long click behavior
      * @param view item cell view
      * @param position position in list
-     * @return true if
+     * @return true if the callback consumed the long click, false otherwise
      */
     public boolean onItemLongClick(View view, int position);
 }
