411640de73b312ce70c05976c4c8a87d0027f4f0
Roderick Lan <rodericklan03@gmail.com>
2023-11-22 13:45:32 -0700

    got rid of useless change

--- app/src/main/java/com/example/boeing301house/itemview/ItemViewActivity.java
+++ app/src/main/java/com/example/boeing301house/itemview/ItemViewActivity.java
@@ -420,9 +420,7 @@ public class ItemViewActivity extends AppCompatActivity implements AddEditItemFr
         for (Uri photo: drop) {
             if (keep.contains(photo)) {
                 deleteFromFirebase(photo);
-            } else if (keep.size() == 0) {
-                deleteFromFirebase(photo);
-            }
+            } 
         }
 
     }
